488805210b704fd2b86af857179057801e5a29a8
asher <sdsdsd@dsfsdfsdfsd.ru>
2025-07-01 19:48:50 +0300

    commit

--- out/_next/static/cEUCVWccpIBPQugvzxRaJ/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-eb6abc946d549328.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],"/[count]":["static/chunks/pages/[count]-1687166e727329cf.js"],sortedPages:["/","/_app","/_error","/[count]"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
